2413c3de9049da717cc5882e85f0bc64d313b211
reveza <rebeccavezinacote@gmail.com>
2019-11-16 14:45:10 -0800

    client

--- expresstest/routes/users.js
+++ expresstest/routes/users.js
@@ -5,11 +5,12 @@ let db = require('../db').getDb;
 /* GET users listing. */
 router.get('/', function(req, res, next) {
   db().all('SELECT rowid AS id, info FROM lorem', function(err, rows) {
-    let result = "";
-    for(var row of rows) {
-      result += row.id + ': ' + row.info + '<br/>';
-    }
-    res.send(result);
+    let result = {};
+    rows.forEach((row) => {
+      result += '{' + row.id + ': ' + row.info + '}';
+    });
+
+    res.send([1,2,3]);
   });
 });
 
